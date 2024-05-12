fe7c6bf7c741362692f0e4839c50665a0908f5fe
Giuseppe Perrotta <gperrotta03.2@gmail.com>
2024-05-13 00:31:56 +0200

    Fixed broadcast

--- simply-command-module/src/main/java/net/giuse/simplycommandmodule/commands/BroadcastCommand.java
+++ simply-command-module/src/main/java/net/giuse/simplycommandmodule/commands/BroadcastCommand.java
@@ -12,10 +12,11 @@ import javax.inject.Inject;
 public class BroadcastCommand extends AbstractCommand {
 
     private final MessageBuilder messageBuilder;
-
+    private final MainModule mainModule;
     @Inject
     public BroadcastCommand(MainModule mainModule) {
         super("broadcast", "lifeserver.broadcast");
+        this.mainModule = mainModule;
         messageBuilder = mainModule.getMessageBuilder();
     }
 
@@ -30,8 +31,11 @@ public class BroadcastCommand extends AbstractCommand {
         for (String arg : args) {
             sb.append(arg).append(" ");
         }
-
-        Bukkit.getOnlinePlayers().forEach(onlinePlayers -> messageBuilder.setCommandSender(commandSender).setIDMessage("broadcast").sendMessage(new TextReplacer().match("%message%").replaceWith(sb.toString())));
+        Bukkit.getOnlinePlayers().forEach(onlinePlayers ->
+                messageBuilder.
+                setCommandSender(onlinePlayers)
+                .setIDMessage("broadcast")
+                .sendMessage(new TextReplacer().match("%message%").replaceWith(sb.toString())));
 
     }
 }
