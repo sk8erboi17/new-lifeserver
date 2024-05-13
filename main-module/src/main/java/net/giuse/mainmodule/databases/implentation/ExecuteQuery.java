c3593ad22e6e69a526e4b047edc1516e6c781945
Giuseppe Perrotta <gperrotta03.2@gmail.com>
2024-05-13 15:21:27 +0200

    Added postgres connector

---
+++ main-module/src/main/java/net/giuse/mainmodule/databases/implentation/ExecuteQuery.java
@@ -0,0 +1,42 @@
+package net.giuse.mainmodule.databases.implentation.h2;
+
+
+import lombok.SneakyThrows;
+import net.giuse.mainmodule.MainModule;
+import net.giuse.mainmodule.databases.execute.Callback;
+import org.bukkit.Bukkit;
+
+import javax.inject.Inject;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ExecuteQuery {
+    private final MainModule mainModule;
+
+
+    @Inject
+    @SneakyThrows
+    public ExecuteQuery(MainModule mainModule) {
+        this.mainModule = mainModule;
+    }
+
+    @SneakyThrows
+    public void execute(Callback callback, String query) {
+        try (PreparedStatement preparedStatement = mainModule.getConnector().getConnection().prepareStatement(query)) {
+            callback.setQuery(preparedStatement);
+        } catch (SQLException e) {
+            Bukkit.getLogger().info("Empty Database");
+        }
+    }
+
+
+    @SneakyThrows
+    public void execute(String query) {
+        try (PreparedStatement preparedStatement = mainModule.getConnector().getConnection().prepareStatement(query)) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            Bukkit.getLogger().info("Empty Database");
+        }
+    }
+
+}
