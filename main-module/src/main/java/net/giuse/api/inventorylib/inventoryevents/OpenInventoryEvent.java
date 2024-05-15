571689a5f9a31c5fec9a751bfa46002ad8dc65bf
skater boi <125193665+sk8erboi17@users.noreply.github.com>
2024-05-15 10:08:20 +0200

    Delete main-module directory

--- main-module/src/main/java/net/giuse/api/inventorylib/inventoryevents/OpenInventoryEvent.java
+++
@@ -1,8 +0,0 @@
-package net.giuse.api.inventorylib.inventoryevents;
-
-import org.bukkit.event.inventory.InventoryOpenEvent;
-
-public interface OpenInventoryEvent {
-
-    void open(InventoryOpenEvent e);
-}
--- main-module/src/main/java/net/giuse/mainmodule/gui/ItemInitializer.java
+++
@@ -1,8 +0,0 @@
-package net.giuse.mainmodule.gui;
-
-
-import net.giuse.api.inventorylib.InventoryBuilder;
-
-public interface ItemInitializer {
-    void initItems(InventoryBuilder inventoryBuilder);
-}
